f063173146b145c92bc34ffa4158cd80a37722af
mahlen <malink027@gmail.com>
2025-03-27 11:56:49 +1100

    flushed the repo

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import CompaniesBanner from "@/components/CompaniesBanner";
-
-export default function NotificationPage() {
-  return (
-    <main className="flex items-center justify-center h-screen">
-      <CompaniesBanner />
-    </main>
-  );
-}
--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface Agent {
-  id: number;
-  name: string;
-  price: number;
-  availability: number;
-  latency: number;
-  trust: number;
-  highlight?: boolean;
-}
